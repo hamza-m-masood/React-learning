df2ac6ba0abe967916037eded492eccaf2d1dafc
HamzaMasood1 <hamzamasood183@gmail.com>
2020-06-11 06:50:59 +0100

    Created boilerplate and Expensify app

---
+++ expensify-app/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/styles.scss'
+import 'normalize.css/normalize.css'
+
+ReactDOM.render(<p>This is my boiler plate</p>, document.getElementById('app'))
