dd0636a87f2f4adb7b9fecc5ebecfc3a8a21cca5
HamzaMasood1 <hamzamasood183@gmail.com>
2020-06-13 01:19:52 +0100

    destructuring in es6

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,6 @@
+
+//OBJECT DESTRUCTURING
+
 console.log("destructuring")
 const person = {
     name: "andrew",
@@ -10,4 +13,37 @@ const person = {
 
 const { name, age } = person
 
-console.log(`${name} is ${age}`) 
+console.log(`${name} is ${age}`)
+
+//destructuing object and using a different name instead of name and age
+
+const { name: namee, age: howOld } = person
+console.log(`${namee} is ${howOld}`)
+
+//setting default values for variable if it does not exist in object
+
+const { city = "hyuuuuu", temp: temperature = 23 } = person.location
+console.log(`${city} is ${temperature}`)
+
+//you can also rename and assign default value
+
+const book = {
+    title: 'Ego is the Enemy',
+    authoer: 'Ryan Holiday',
+    publisher: {
+        name: 'penguin'
+    }
+}
+
+const { name: publisherName = "self-published" } = book.publisher
+console.log(publisherName)
+
+
+
+////////ARRAY DESTRUCTURING
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+
+// const [street, city, state, zip] = address
+
+// console.log(`You are in ${address[1]} ${address[2]}`)
